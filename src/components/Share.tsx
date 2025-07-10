f93a19410d68095072507cecb6fc8d9d675973b5
dogman0121 <giachetti101@gmail.com>
2025-07-10 21:29:15 +0300

    v2 prototype: add lists

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -44,7 +44,7 @@ export default function Share({open, onClose, link}: {link: string, open: boolea
                     bgcolor: theme.palette.background.paper,
                     borderRadius: "12px",
                     p: theme.spacing(3),
-                    width: "400px"
+                    width: "min(400px, 100vw)"
                 }}
             >
                 <Box
